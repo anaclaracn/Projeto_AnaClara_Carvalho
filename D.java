417467567e552738a8799ad09d7ba8291d50d3e7
Ana Clara Carvalho <ana.nascimento12@estudante.ufla.br>
2024-10-25 14:21:44 -0300

    Criação da Classe D

---
+++ D.java
@@ -0,0 +1,17 @@
+public class D {
+    private String D1;
+    private int D2;
+
+    public D(String D1, int D2) {
+        this.D1 = D1;
+        this.D2 = D2;
+    }
+
+    public void MD1() {
+        System.out.println("MD1");
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
+}
