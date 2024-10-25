6db0724df6e5d9ec7d5b5a1637552ae587d8f3aa
Ana Clara Carvalho <ana.nascimento12@estudante.ufla.br>
2024-10-24 23:21:44 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,17 @@
+public class C {
+    private String C1;
+    private int C2;
+
+    public C(String C1, int C2) {
+        this.C1 = C1;
+        this.C2 = C2;
+    }
+
+    public void MC1() {
+        System.out.println("MC1");
+    }
+
+    public void MC2() {
+        System.out.println("MC2");
+    }
+}
